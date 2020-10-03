f847fc3d673f67d7e71c2a79c3fc19fcd16678f8
Szőcs Áron <szum7@hotmail.com>
2020-10-03 22:18:54 +0200

    Run to 4 directions with animation

---
+++ Assets/Scripts/BasicMovement_v2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement_v2 : MonoBehaviour {
+
+    public Animator animator;
+    public float speed;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        //Vector3 vertical = new Vector3(Input.GetAxis("Vertical"), 0.0f, 0.0f);
+        transform.position += horizontal * speed;
+    }
+}
