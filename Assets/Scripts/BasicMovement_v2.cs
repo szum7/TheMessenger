dcff8bd589f0762c3e009ac774350ac3223368e4
Szőcs Áron <szum7@hotmail.com>
2020-10-04 12:15:58 +0200

    Movement v2

--- Assets/Scripts/BasicMovement_v2.cs
+++ Assets/Scripts/BasicMovement_v2.cs
@@ -2,22 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BasicMovement_v2 : MonoBehaviour {
-
+public class BasicMovement_v2 : MonoBehaviour
+{
     public Animator animator;
     public float speed;
 
-    void Start() {
-        
+    void Start()
+    {
     }
 
-    void Update() {
+    void Update()
+    {
+        var movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+
+    void Movement_v1()
+    {
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
 
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
-        //Vector3 vertical = new Vector3(Input.GetAxis("Vertical"), 0.0f, 0.0f);
         transform.position += horizontal * speed;
     }
 }
